6e5608005a96762b5b847f93d570f1941a4294cb
Paolo Bertoli <paolo.bertoli@blazargroup.com>
2025-02-17 15:32:35 +0100

    fix path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import { useState, useEffect, useRef } from "react";
-import { motion } from "framer-motion";
-import { FaSearch, FaMoon, FaSun, FaArrowRight, FaCopy } from "react-icons/fa";
+import {useEffect, useRef, useState} from "react";
+import {motion} from "framer-motion";
+import {FaArrowRight, FaCopy, FaMoon, FaSearch, FaSun} from "react-icons/fa";
 import Cookies from "js-cookie";
 
 export default function App() {
@@ -17,9 +17,10 @@ export default function App() {
         return Cookies.get("darkMode") === "true";
     });
     const inputRef = useRef(null);
+    const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
 
     useEffect(() => {
-        Cookies.set("darkMode", darkMode, { expires: 365 });
+        Cookies.set("darkMode", darkMode, {expires: 365});
     }, [darkMode]);
 
     useEffect(() => {
@@ -60,7 +61,7 @@ export default function App() {
         }
 
         try {
-            const response = await fetch(`http://localhost:5000/acestream?term=${encodeURIComponent(searchTerm)}`);
+            const response = await fetch(`${BASE_URL}/acestream?term=${encodeURIComponent(searchTerm)}`);
             const data = await response.json();
             setResults(data);
             setSearched(true);
@@ -90,20 +91,24 @@ export default function App() {
     };
 
     return (
-        <div className={`flex flex-col items-center min-h-screen p-4 select-none ${darkMode ? 'bg-gray-950 text-white' : 'bg-gray-100 text-black'}`}>
+        <div
+            className={`flex flex-col items-center min-h-screen p-4 select-none ${darkMode ? 'bg-gray-950 text-white' : 'bg-gray-100 text-black'}`}>
             <div className="w-full flex justify-end p-4">
                 <button onClick={() => setDarkMode(!darkMode)} className="text-xl focus:outline-none">
-                    {darkMode ? <FaSun className="text-purple-600" /> : <FaMoon className="text-purple-600" />}
+                    {darkMode ? <FaSun className="text-purple-600"/> : <FaMoon className="text-purple-600"/>}
                 </button>
             </div>
             <motion.div
-                initial={{ y: window.innerWidth < 768 ? "20vh" : "30vh" }}
-                animate={{ y: `${barPosition}vh` }}
-                transition={{ duration: searched ? 0.4 : mobileMoving || desktopSecondSearch ? 1.5 : 2.5, ease: "easeOut" }}
+                initial={{y: window.innerWidth < 768 ? "20vh" : "30vh"}}
+                animate={{y: `${barPosition}vh`}}
+                transition={{
+                    duration: searched ? 0.4 : mobileMoving || desktopSecondSearch ? 1.5 : 2.5,
+                    ease: "easeOut"
+                }}
                 className={`w-full max-w-xl ${darkMode ? 'bg-gray-800' : 'bg-white'} p-4 rounded-full flex items-center shadow-lg focus-within:ring-2 focus-within:ring-purple-600 transition border border-gray-300 relative shadow-[0px_4px_10px_rgba(0,0,0,0.1)]`}
             >
                 <motion.div className="ml-4 opacity-50">
-                    <FaSearch className="text-2xl text-purple-600" />
+                    <FaSearch className="text-2xl text-purple-600"/>
                 </motion.div>
                 <div className="w-full flex">
                     <input
@@ -120,7 +125,7 @@ export default function App() {
                     {loading ? (
                         <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-purple-600"></div>
                     ) : (
-                        <FaArrowRight className="text-2xl text-purple-600 cursor-pointer" onClick={handleSearch} />
+                        <FaArrowRight className="text-2xl text-purple-600 cursor-pointer" onClick={handleSearch}/>
                     )}
                 </div>
             </motion.div>
@@ -130,16 +135,17 @@ export default function App() {
                     {results.map((source, index) => (
                         <motion.div
                             key={index}
-                            initial={{ opacity: 0, y: 20 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 0.4, delay: index * 0.1 }}
+                            initial={{opacity: 0, y: 20}}
+                            animate={{opacity: 1, y: 0}}
+                            transition={{duration: 0.4, delay: index * 0.1}}
                             className={`p-4 mb-4 rounded-xl shadow-lg ${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'} border border-gray-600 transform transition-transform hover:scale-[1.03] hover:shadow-[0px_6px_14px_rgba(0,0,0,0.3)] shadow-[0px_4px_10px_rgba(0,0,0,0.1)]`}
                         >
                             <p className="text-gray-400 text-sm font-semibold">{source.game_title || "Senza titolo"}</p>
                             <ul className="mt-2">
                                 {source.acestream_links.length > 0 ? (
                                     source.acestream_links.map((link, i) => (
-                                        <li key={i} className="mt-2 flex justify-between items-center text-ellipsis overflow-hidden">
+                                        <li key={i}
+                                            className="mt-2 flex justify-between items-center text-ellipsis overflow-hidden">
                                             <a
                                                 href={link.link}
                                                 className="text-purple-500 hover:underline truncate"
@@ -148,7 +154,8 @@ export default function App() {
                                             >
                                                 {link.language ? `${link.language} - ` : ""} {link.link}
                                             </a>
-                                            <FaCopy className="text-purple-600 cursor-pointer ml-2" onClick={() => handleCopy(link.link)} />
+                                            <FaCopy className="text-purple-600 cursor-pointer ml-2"
+                                                    onClick={() => handleCopy(link.link)}/>
                                         </li>
                                     ))
                                 ) : (
